6afd69e07f00fe2183ab120d7eb9fd79d180b2df
Devesh Yadav <deveshyadav.shubh@gmail.com>
2026-02-18 18:41:13 +0530

    Task-199-fileupload-commonservice

---
+++ src/main/java/nimblix/in/HealthCareHub/controller/DoctorController.java
@@ -0,0 +1,21 @@
+package nimblix.in.HealthCareHub.controller;
+
+import lombok.RequiredArgsConstructor;
+import nimblix.in.HealthCareHub.helper.UploadImageHelper;
+import nimblix.in.HealthCareHub.response.MultipleImageResponse;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctor")
+@RequiredArgsConstructor
+public class DoctorController {
+
+    private final UploadImageHelper uploadImageHelper;
+
+    @PostMapping("/upload")
+    public MultipleImageResponse uploadFiles(@RequestParam("files") List<MultipartFile> files) throws Exception {
+        return uploadImageHelper.uploadImages(files);
+    }
+}
