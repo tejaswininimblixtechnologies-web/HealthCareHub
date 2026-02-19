03f3be1226390e1f6f52ef019a7f6a793d3ea86d
Deepa15-V <hiremathdeepu15@gmail.com>
2026-02-19 12:01:03 +0530

    Added bill Item

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/constants/HealthCareConstants.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/constants/HealthCareConstants.java
@@ -2,4 +2,7 @@ package nimblix.in.HealthCareHub.constants;
 
 public class HealthCareConstants {
 
+    public static final String STATUS_SUCCESS = "SUCCESS";
+    public static final String STATUS_ERROR = "ERROR";
+
 }
