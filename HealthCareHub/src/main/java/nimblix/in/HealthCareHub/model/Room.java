5a65a61480338f39227811c435cb379c6d61c0ae
kusuma <kusumagowda1318@gmail.com>
2026-02-19 18:38:40 +0530

    Get Available Rooms

---
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/model/Room.java
@@ -0,0 +1,18 @@
+package nimblix.in.HealthCareHub.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomNumber;
+    private String roomType;
+    private boolean available;
+
+    // getters and setters
+}
