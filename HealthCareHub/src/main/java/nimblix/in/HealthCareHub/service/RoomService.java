89f3a1e4151bbeacea5a2f110b24a7b02fa16bad
kusuma <kusumagowda1318@gmail.com>
2026-02-21 10:44:03 +0530

    Get Available Rooms

---
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/service/RoomService.java
@@ -0,0 +1,20 @@
+package nimblix.in.HealthCareHub.service;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import nimblix.in.HealthCareHub.model.Room;
+import nimblix.in.HealthCareHub.repository.RoomRepository;
+
+@Service
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAvailableRooms() {
+        return roomRepository.findByAvailableTrue();
+    }
+}
