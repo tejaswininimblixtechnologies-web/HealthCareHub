8e9f8159c3ca80dd7963d7cc3b8cc52c400a048b
pavithrasherugar <bspavithra2001@gmail.com>
2026-02-24 17:51:29 +0530

    Delete HealthCareHub/src/main/java/nimblix/in/HealthCareHub/service/HospitalService.java

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/service/HospitalService.java
+++
@@ -1,8 +0,0 @@
-package nimblix.in.HealthCareHub.service;
-
-import nimblix.in.HealthCareHub.request.HospitalRegistrationRequest;
-
-public interface HospitalService {
-
-    String registerHospital(HospitalRegistrationRequest request);
-}
