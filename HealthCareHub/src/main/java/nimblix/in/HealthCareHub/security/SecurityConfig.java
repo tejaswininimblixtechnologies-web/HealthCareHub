f0731fdcdbebae07407f20220c3c51d23b3051c2
basavaraj636 <baswabasavaraj07@gmail.com>
2026-02-24 14:26:47 +0530

    General User Registration
    The General User Registration functionality.
    Created AuthConroller with /api/auth/register endpoint.
    Implemented duplicate email validation.
    Added password encoded using PasswordEncoder. default role is user and saved user details in database.
    APIs: POST /api/auth/register
    Tested using postman and verified duplicate email validation, password encryption and successful user creation.

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/security/SecurityConfig.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/security/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/medicines/**"
+                                "/medicines/**",
+                                "/api/auth/**",
                                 "/api/doctors/**"
                         ).permitAll()
                         .anyRequest().authenticated()
