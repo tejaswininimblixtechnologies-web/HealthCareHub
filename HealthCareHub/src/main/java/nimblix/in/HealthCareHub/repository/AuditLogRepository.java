3e11508b51131417988e7c4a176dc39a057faa8d
pavithrasherugar <bspavithra2001@gmail.com>
2026-02-24 17:42:47 +0530

    Create AuditLogRepository.java

---
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/repository/AuditLogRepository.java
@@ -0,0 +1,8 @@
+package nimblix.in.HealthCareHub.repository;
+
+import nimblix.in.HealthCareHub.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
+
