1244311b446bf9b006faee0b79cd7ed1da568bd0
kusuma <kusumagowda1318@gmail.com>
2026-02-26 10:31:22 +0530

    List all medical specializations

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/HospitalController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/HospitalController.java
@@ -1,23 +1,32 @@
 package nimblix.in.HealthCareHub.controller;
 
 import lombok.RequiredArgsConstructor;
+import nimblix.in.HealthCareHub.model.Specialization;
 import nimblix.in.HealthCareHub.request.HospitalRegistrationRequest;
 import nimblix.in.HealthCareHub.service.HospitalService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nimblix.in.HealthCareHub.service.SpecializationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/hospital")
+@RequestMapping("/api/hospital")
 @RequiredArgsConstructor
 public class HospitalController {
 
     private final HospitalService hospitalService;
+    private final SpecializationService specializationService;
+
 
     @PostMapping("/register")
     public String registerHospital(@RequestBody HospitalRegistrationRequest request) {
         return hospitalService.registerHospital(request);
     }
-    
+
+    //  Get all specializations (inside HospitalController)
+    @GetMapping("/specializations")
+    public ResponseEntity<List<Specialization>> getAllSpecializations() {
+        return ResponseEntity.ok(specializationService.getAllSpecializations());
+    }
 }
