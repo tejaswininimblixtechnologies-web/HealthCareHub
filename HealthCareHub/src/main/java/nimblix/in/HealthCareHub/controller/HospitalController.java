ec52cb2c5611d93bbf9ea762f4cde772b50d3f30
UDAYCHANDRA ANDRA <udayandranimblix@gmail.com>
2026-02-26 12:36:08 +0530

    Add existing HospitalController code

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/HospitalController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/HospitalController.java
@@ -1,6 +1,12 @@
 package nimblix.in.HealthCareHub.controller;
 
 import lombok.RequiredArgsConstructor;
+import nimblix.in.HealthCareHub.request.HospitalRegistrationRequest;
+import nimblix.in.HealthCareHub.service.HospitalService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import nimblix.in.HealthCareHub.request.UserFilterRequest;
 import nimblix.in.HealthCareHub.response.UserResponse;
 import nimblix.in.HealthCareHub.service.UserService;
@@ -12,9 +18,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/hospital")
 @RequiredArgsConstructor
 public class HospitalController {
-
+    private final HospitalService hospitalService;
     private final UserService userService;
 
+
+    @PostMapping("/register")
+    public String registerHospital(@RequestBody HospitalRegistrationRequest request) {
+        return hospitalService.registerHospital(request);
+    }
     @GetMapping("/users")
     public Page<UserResponse> getUsers(
             @RequestParam(defaultValue = "0") int page,
