97692ec16402e408c0c54cb4195bbd1c4861ac87
nithinrajkumar559-blip <nithinrajkumarnimblix@outlook.com>
2026-03-02 13:37:22 +0530

    fix: moved prescription endpoint to PatientController and secured DB password

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/PatientController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/PatientController.java
@@ -1,11 +1,25 @@
 package nimblix.in.HealthCareHub.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import nimblix.in.HealthCareHub.service.PrescriptionService;
+import nimblix.in.HealthCareHub.model.Prescription;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/patient")
 @RequiredArgsConstructor
 public class PatientController {
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    // Add this endpoint method:
+    @GetMapping("/{patientId}/prescriptions")
+    public List<Prescription> getPrescriptionsByPatient(@PathVariable Long patientId) {
+        return prescriptionService.getPrescriptionsByPatientId(patientId);
+    }
 }
