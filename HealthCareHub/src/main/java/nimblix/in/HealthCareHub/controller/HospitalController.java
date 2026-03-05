ca0f0593565cf2aa612f0fc232884480cf523f05
sinchanadr712 <sinchanadr712@gmail.com>
2026-03-06 00:03:56 +0530

    Pagination Helper Class so all APIs use the same pagination logic.

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/HospitalController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/HospitalController.java
@@ -1,37 +1,33 @@
+// controller/HospitalController.java
 package nimblix.in.HealthCareHub.controller;
 
 import lombok.RequiredArgsConstructor;
-
 import nimblix.in.HealthCareHub.request.HospitalRegistrationRequest;
 import nimblix.in.HealthCareHub.response.PaginatedMedicineResponse;
 import nimblix.in.HealthCareHub.service.HospitalService;
 import nimblix.in.HealthCareHub.service.MedicineService;
 import org.springframework.web.bind.annotation.*;
-import nimblix.in.HealthCareHub.response.MedicineResponse;
-
 
 @RestController
-@RequestMapping("api/hospital")
+@RequestMapping("/api/hospital")
 @RequiredArgsConstructor
 public class HospitalController {
 
     private final HospitalService hospitalService;
     private final MedicineService medicineService;
 
+    // REGISTER HOSPITAL
     @PostMapping("/register")
     public String registerHospital(@RequestBody HospitalRegistrationRequest request) {
         return hospitalService.registerHospital(request);
-
-
     }
-
 
     // GET ALL MEDICINES (PAGINATED)
     @GetMapping("/medicines")
-    public PaginatedMedicineResponse  getAllMedicines (
+    public PaginatedMedicineResponse getAllMedicines(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
+            @RequestParam(defaultValue = "10") int size) {
+
         return medicineService.getAllMedicines(page, size);
     }
 }
--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/serviceImpl/MedicineServiceImpl.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/serviceImpl/MedicineServiceImpl.java
@@ -1,36 +1,36 @@
+
 package nimblix.in.HealthCareHub.serviceImpl;
 
 import lombok.RequiredArgsConstructor;
+import nimblix.in.HealthCareHub.helper.PaginationHelper;
+import nimblix.in.HealthCareHub.model.Medicine;
+
 import nimblix.in.HealthCareHub.repository.MedicineRepository;
 import nimblix.in.HealthCareHub.response.MedicineResponse;
 import nimblix.in.HealthCareHub.response.PaginatedMedicineResponse;
 import nimblix.in.HealthCareHub.service.MedicineService;
-import nimblix.in.HealthCareHub.utility.PaginationUtil;
-
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 @Service
 @RequiredArgsConstructor
 public class MedicineServiceImpl implements MedicineService {
 
-    private final MedicineRepository repository;
+    private final MedicineRepository medicineRepository;
 
     @Override
     public PaginatedMedicineResponse getAllMedicines(int page, int size) {
 
-        Pageable pageable = PageRequest.of(page, size);
-
-        // Fetch DTO directly from DB
         Page<MedicineResponse> medicinePage =
-                repository.findAllMedicineResponses(pageable);
+                medicineRepository.findAllMedicineResponses(
+                        PaginationHelper.getPageable(page, size));
 
-        //Use pagination helper
-        return PaginationUtil.buildMedicineResponse(
-                medicinePage,
-                medicinePage.getContent()
-        );
+        return PaginatedMedicineResponse.builder()
+                .status("SUCCESS")
+                .message("Medicines fetched successfully")
+                .medicines(medicinePage.getContent())
+                .currentPage(medicinePage.getNumber())
+                .totalPages(medicinePage.getTotalPages())
+                .totalElements(medicinePage.getTotalElements())
+                .build();
     }
 }
